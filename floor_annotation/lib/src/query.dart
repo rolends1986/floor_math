51cfaae233349d955b5f69b0a1e80d0a0b7cd1c0
Ching <133961963322@163.com>
2019-07-16 11:13:41 +0800

    新增固定替换值定义

--- floor_annotation/lib/src/query.dart
+++ floor_annotation/lib/src/query.dart
@@ -1,8 +1,10 @@
 /// Marks a method as a query method.
 class Query {
+  final List<String> constFields;
+
   /// The SQLite query.
   final String value;
 
   /// Marks a method as a query method.
-  const Query(this.value);
+  const Query(this.value, {this.constFields});
 }
